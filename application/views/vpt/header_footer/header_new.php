335218b7724632f66e9ffc98a863285730dd1352
Colin Ogoo <ogoo.colin@gmail.com>
2016-03-08 21:21:19 +0000

    new header

--- application/views/vpt/header_footer/header_new.php
+++ application/views/vpt/header_footer/header_new.php
@@ -5,7 +5,10 @@
         <meta http-equiv="x-ua-compatible" content="ie=edge">
         <title></title>
         <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+        <meta name="apple-touch-fullscreen" content="yes" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black" />
 
         <link rel="apple-touch-icon" href="apple-touch-icon.png">
         <!-- Place favicon.ico in the root directory -->
